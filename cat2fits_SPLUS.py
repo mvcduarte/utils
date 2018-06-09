4235ec77aacf3672d8f01ab1468f27ba8ff58330
Marcus <mvcduarte@yahoo.com.br>
2018-06-09 10:54:05 -0300

    Update cat2fits_SPLUS.py

--- cat2fits_SPLUS.py
+++ cat2fits_SPLUS.py
@@ -6,18 +6,21 @@
                     Costa-Duarte, M. V. - 08/06/2018
 
 """
+
+from __future__ import print_function
 from astropy.io import fits, ascii
 import numpy as np
 import sys
+
 # Input file (ASCII)
 infile_cat = str(sys.argv[1]) #'SPLUS_STRIPE82-0003_photo_BPZ.cat'
+
 # Output file (FITS)
 outfile_fits = infile_cat.replace('.cat', '.fits')
 
 if __name__ == '__main__':
-
-    
-	# Loading ASCII catalogue
+	
+    # Loading ASCII catalogue
     
     print("Loading...%s" % infile_cat)
     data = ascii.read(infile_cat)
