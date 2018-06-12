1978871394790cb3cfb4b5262beefe5639536684
Marcus <mvcduarte@yahoo.com.br>
2018-06-12 10:55:34 -0300

    Update cat2fits_SPLUS.py

--- cat2fits_SPLUS.py
+++ cat2fits_SPLUS.py
@@ -15,19 +15,19 @@ import glob
 
 if __name__ == '__main__':
 
+    # List of catalogue files at ./
+      
     list_cat = glob.glob('SPLUS_STRIPE82-000*.cat')
 
-    for i in range(len(list_cat)):
+    for i in range(len(list_cat)): # loop over all catalogue files
 
-        # Output file (FITS)
+        # Define output file (FITS)
         outfile_fits = list_cat[i].replace('.cat', '.fits')
     	
         # Loading ASCII catalogue
-        
         print("Loading...%s" % list_cat[i])
         data = ascii.read(list_cat[i])
 
         # Saving FITS file
-
         print("Saving...%s" % outfile_fits)
         data.write(outfile_fits, format = 'fits', overwrite = True)
